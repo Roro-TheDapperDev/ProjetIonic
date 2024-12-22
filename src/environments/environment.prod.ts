8ae5dd5d3d2dc5bcf838c79af0b135da48e0087e
Rodney Meyer <rodney.meyer4@etu.univ-lorraine.fr>
2024-12-22 17:49:45 +0100

    changements pour build android réussi

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,19 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyCrjHsG1jDwo8SAw1-AHAvAJByJRSOItQE",
+
+  authDomain: "geocaching-b3293.firebaseapp.com",
+
+  projectId: "geocaching-b3293",
+
+  storageBucket: "geocaching-b3293.firebasestorage.app",
+
+  messagingSenderId: "1090858581213",
+
+  appId: "1:1090858581213:web:1f82bdac298799cc722f34",
+
+  measurementId: "G-QZSQMMB9GS"
+
+  }
 };
